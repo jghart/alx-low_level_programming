b86f312b116799ed28e20d6b9da9e4fa19137bdc
jghart <appiahjoe88@gmail.com>
2022-11-10 04:14:25 +0000

    Argument tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * @argv: argument variable
  * Return: 0 on success
  */
-int main(int argc __attribute__((unused)), int argv[])
+int main(int argc __attribute__((unused)), int *argv[])
 {
-	printf("%s\n", argv[]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		for (sum = atoi(argv[1]) * atoi(argv[2]))
-			printf("%d", sum);
+	
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
-	}
 }
